ef3734842bcb7105ab4d765bde0cb31e3cc94565
Liju Jose <liju.jose@gds.ey.com>
2019-06-18 16:19:36 +0530

    feat(API-Gateway): Added integration test for token

--- API-Gateway/tests/__test.js
+++ API-Gateway/tests/__test.js
@@ -1,8 +1,20 @@
+ global.generateRandomSerial = function () {
+  const seed = "0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF012345";
+  let serialNumber = "0x";
+  for(var index = 0; index < 54; index++) {
+    serialNumber += seed.charAt(Math.floor(Math.random() * seed.length));
+  }
+  console.log("Serial Number ::: " + serialNumber);
+  return serialNumber;
+};
 
-//require('./api-gateway.test');
+require('./api-gateway.test');
 require('./ft.test');
 require('./nft.test');
+require('./token.test');
 //require('./teardown');
 
 //Seethu: ""
 //
+
+
