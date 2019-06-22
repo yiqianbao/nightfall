51b7bfc2b9b371e6558c33cdd248957eca53f71a
Liju Jose <liju.jose@gds.ey.com>
2019-06-22 14:15:42 +0530

    refactor: indentation corrected to space 2

--- API-Gateway/tests/coin.test.js
+++ API-Gateway/tests/coin.test.js
@@ -8,166 +8,161 @@ let coinTwo = {};
 let changeToken = {};
 
 describe('Suite for ERC-20 Token Commitments', function() {
-     
-    before((done) =>{
-        /**
-         * Login to Alice account
-         */
-        let aliceAuth = request(domainName)
-            .post('/login')
-            .send(aliceDetails)
-            .set('Accept', 'application/json');
-        /**
-         * Login to Bob account
-         */    
-        let bobAuth = request(domainName)
-            .post('/login')
-            .send(bobDetails)
-            .set('Accept', 'application/json');   
-        /**
-         * Set account tokens. 
-         * */    
-        Promise.all([aliceAuth, bobAuth]).then(async (resp) =>{
-            let aliceRes = resp[0];
-            let bobRes = resp[1];
-            let aliceBody = aliceRes.body.data;
-            let bobBody = bobRes.body.data;
-            Object.assign(aliceDetails,{pk: aliceBody.publickey, sk: aliceBody.secretkey, token: aliceBody.token, address: aliceBody.address});
-            Object.assign(bobDetails,{pk: bobBody.publickey, sk: bobBody.secretkey, token: bobBody.token, address: bobBody.address});
-            await request(domainName)
-                .post('/ft/mint')
-                .send({amount:ft.mintAmount})
-                .set('Accept', 'application/json')
-                .set('Authorization', aliceDetails.token);
-            done();
-        });
 
+  before((done) =>{
+    /**
+    * Login to Alice account
+    */
+    let aliceAuth = request(domainName)
+      .post('/login')
+      .send(aliceDetails)
+      .set('Accept', 'application/json');
+    /**
+    * Login to Bob account
+    */    
+    let bobAuth = request(domainName)
+      .post('/login')
+      .send(bobDetails)
+      .set('Accept', 'application/json');   
+    /**
+    * Set account tokens. 
+    * */    
+    Promise.all([aliceAuth, bobAuth]).then(async (resp) =>{
+      let aliceRes = resp[0];
+      let bobRes = resp[1];
+      let aliceBody = aliceRes.body.data;
+      let bobBody = bobRes.body.data;
+      Object.assign(aliceDetails,{pk: aliceBody.publickey, sk: aliceBody.secretkey, token: aliceBody.token, address: aliceBody.address});
+      Object.assign(bobDetails,{pk: bobBody.publickey, sk: bobBody.secretkey, token: bobBody.token, address: bobBody.address});
+      await request(domainName)
+        .post('/ft/mint')
+        .send({amount:ft.mintAmount})
+        .set('Accept', 'application/json')
+        .set('Authorization', aliceDetails.token);
+        done();
     });
+  });
 
-    describe('POST /coin/mint', function () {
+  describe('POST /coin/mint', function () {
 
-        it(`should Mint ERC20 Token commitment of value ${ft_commitment.mintTokenOne} for Alice`, function (done) {
-            let body = {
-                 A: convertToHex(ft_commitment.mintTokenOne),
-                 pk_A: aliceDetails.pk,
-                 S_A: generateRandomSerial()
-            };
-            request(domainName)
-                .post('/coin/mint')
-                .send(body)
-                .set('Accept', 'application/json')
-                .set('Authorization', aliceDetails.token)
-                .expect('Content-Type', /json/)
-                .expect(200)
-                .end((err, resp) => {
-                    if (err) return done(err);
-                    coinOne = Object.assign({}, resp.body.data, body);
-                    done();
-                });
-        });
-
-        it(`should Mint ERC20 Token commitment of value ${ft_commitment.mintTokenTwo} for Alice`, function (done) {
-            let body = {
-                 A: convertToHex(ft_commitment.mintTokenTwo),
-                 pk_A: aliceDetails.pk,
-                 S_A: generateRandomSerial()
-            };
-            request(domainName)
-                .post('/coin/mint')
-                .send(body)
-                .set('Accept', 'application/json')
-                .set('Authorization', aliceDetails.token)
-                .expect('Content-Type', /json/)
-                .expect(200)
-                .end((err, resp) => {
-                    if (err) return done(err);
-                    coinTwo = {...resp.body.data, ...body};
-                    done();
-                });
-        });
-
-        
-    });
-
-    describe('POST /coin/transfer', function () { 
-        it(`should transfer ERC20 token commitment of value ${ft_commitment.transferAmount} from Alice to Bob`, function (done) {
-            this.timeout(500000);
-            let body = {
-               C: coinOne.A,
-               D: coinTwo.A,
-               E: convertToHex(ft_commitment.transferAmount),
-               F: getBalance(coinOne.A, coinTwo.A, ft_commitment.transferAmount),
-               S_C: coinOne.S_A,
-               S_D: coinTwo.S_A,
-               z_C_index: coinOne.coin_index,
-               z_D_index: coinTwo.coin_index,
-               S_E: generateRandomSerial(),
-               S_F: generateRandomSerial(),
-               sk_A: aliceDetails.sk,
-               z_C: coinOne.coin,
-               z_D: coinTwo.coin,
-               pk_A: aliceDetails.pk,
-               receiver_name: bobDetails.name,
-               pk_B: bobDetails.pk
-            };
-            request(domainName)
-                .post('/coin/transfer')
-                .send(body)
-                .set('Accept', 'application/json')
-                .set('Authorization', aliceDetails.token)
-                .expect('Content-Type', /json/)
-                .expect(200)
-                .end((err, resp) => {
-                    if (err) return done(err);
-                    Object.assign(changeToken, resp.body.data, {S_F: body.S_F});
-                    done();
-                });
+    it(`should Mint ERC20 Token commitment of value ${ft_commitment.mintTokenOne} for Alice`, function (done) {
+      let body = {
+        A: convertToHex(ft_commitment.mintTokenOne),
+        pk_A: aliceDetails.pk,
+        S_A: generateRandomSerial()
+      };
+      request(domainName)
+        .post('/coin/mint')
+        .send(body)
+        .set('Accept', 'application/json')
+        .set('Authorization', aliceDetails.token)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((err, resp) => {
+          if (err) return done(err);
+          coinOne = Object.assign({}, resp.body.data, body);
+          done();
         });
     });
 
-    describe('GET /coin', function () {
-        it('should get list of ERC20 token commitments of Alice account', function (done) {
-            request(domainName)
-                .get(`/database/coin?address=${aliceDetails.address}`)
-                .set('Accept', 'application/json')
-                .set('Authorization', aliceDetails.token)
-                .expect('Content-Type', /json/)
-                .expect(200)
-                .end((err, resp) => {
-                    if (err) return done(err);
-                    expect(resp.body.data.data).to.have.lengthOf.above(0);
-                    done();
-                });
-        });
-    });
-
-    describe('POST /coin/burn', function () {
-        it(`should burn ERC20 token commitment from Alice \'s account`, function (done) {
-            this.timeout(500000);
-            let body = {
-                A: convertToHex(ft_commitment.burnAmount),
-                sk_A: aliceDetails.sk,
-                S_A: changeToken.S_F,
-                pk_A: aliceDetails.pk,
-                z_A_index: changeToken.z_F_index,
-                z_A: changeToken.z_F
-            };
-
-            request(domainName)
-                .post('/coin/burn')
-                .send(body)
-                .set('Accept', 'application/json')
-                .set('Authorization', aliceDetails.token)
-                .expect('Content-Type', /json/)
-                .expect(200)
-                .end((err) => {
-                    if (err) return done(err);
-                    done();
-                });
+    it(`should Mint ERC20 Token commitment of value ${ft_commitment.mintTokenTwo} for Alice`, function (done) {
+      let body = {
+        A: convertToHex(ft_commitment.mintTokenTwo),
+        pk_A: aliceDetails.pk,
+        S_A: generateRandomSerial()
+      };
+      request(domainName)
+        .post('/coin/mint')
+        .send(body)
+        .set('Accept', 'application/json')
+        .set('Authorization', aliceDetails.token)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((err, resp) => {
+          if (err) return done(err);
+          coinTwo = {...resp.body.data, ...body};
+          done();
         });
     });
 
 
-    
+  });
 
+  describe('POST /coin/transfer', function () { 
+    it(`should transfer ERC20 token commitment of value ${ft_commitment.transferAmount} from Alice to Bob`, function (done) {
+      this.timeout(500000);
+      let body = {
+        C: coinOne.A,
+        D: coinTwo.A,
+        E: convertToHex(ft_commitment.transferAmount),
+        F: getBalance(coinOne.A, coinTwo.A, ft_commitment.transferAmount),
+        S_C: coinOne.S_A,
+        S_D: coinTwo.S_A,
+        z_C_index: coinOne.coin_index,
+        z_D_index: coinTwo.coin_index,
+        S_E: generateRandomSerial(),
+        S_F: generateRandomSerial(),
+        sk_A: aliceDetails.sk,
+        z_C: coinOne.coin,
+        z_D: coinTwo.coin,
+        pk_A: aliceDetails.pk,
+        receiver_name: bobDetails.name,
+        pk_B: bobDetails.pk
+      };
+      request(domainName)
+        .post('/coin/transfer')
+        .send(body)
+        .set('Accept', 'application/json')
+        .set('Authorization', aliceDetails.token)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((err, resp) => {
+          if (err) return done(err);
+          Object.assign(changeToken, resp.body.data, {S_F: body.S_F});
+          done();
+        });
+    });
+  });
+
+  describe('GET /coin', function () {
+    it('should get list of ERC20 token commitments of Alice account', function (done) {
+      request(domainName)
+        .get(`/database/coin?address=${aliceDetails.address}`)
+        .set('Accept', 'application/json')
+        .set('Authorization', aliceDetails.token)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((err, resp) => {
+          if (err) return done(err);
+          expect(resp.body.data.data).to.have.lengthOf.above(0);
+          done();
+        });
+    });
+  });
+
+  describe('POST /coin/burn', function () {
+    it(`should burn ERC20 token commitment from Alice \'s account`, function (done) {
+      this.timeout(500000);
+      let body = {
+        A: convertToHex(ft_commitment.burnAmount),
+        sk_A: aliceDetails.sk,
+        S_A: changeToken.S_F,
+        pk_A: aliceDetails.pk,
+        z_A_index: changeToken.z_F_index,
+        z_A: changeToken.z_F
+      };
+
+      request(domainName)
+        .post('/coin/burn')
+        .send(body)
+        .set('Accept', 'application/json')
+        .set('Authorization', aliceDetails.token)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((err) => {
+          if (err) return done(err);
+          done();
+        });
+    });
+  });
 });
--- API-Gateway/tests/testData.js
+++ API-Gateway/tests/testData.js
@@ -1,28 +1,28 @@
 module.exports = {
-    domainName: 'http://api-test.nightfall.docker',
-    aliceDetails: { name: "alice", email: "alice@ey.com", password: "pass", pk:"",sk:"", token:"", address: ""},
-    bobDetails:{ name: "bob", email: "bob@ey.com", password: "pass", pk:"",sk:"", token:"", address:"" },
-    ft:{
-       mintAmount: 1000,
-       transferAmount:100,
-       burnAmount:50
-    },
-    nft:{
-        tokenUriOne: 'widget01',
-        tokenUriTwo: 'widget02',
-        tokenUriThree:'widget03'
-     },
-     ft_commitment:{
-        mintTokenOne: 10,
-        mintTokenTwo: 20,
-        transferAmount:15,
-        burnAmount:15
-     },
-    databaseConfig:{
-        host: 'nightfall_mongo_1',
-        port: "27017",
-        databaseName: "nightfall",
-        admin: "admin",
-        password: "admin"
-    },
+  domainName: 'http://api-test.nightfall.docker',
+  aliceDetails: { name: "alice", email: "alice@ey.com", password: "pass", pk:"",sk:"", token:"", address: ""},
+  bobDetails:{ name: "bob", email: "bob@ey.com", password: "pass", pk:"",sk:"", token:"", address:"" },
+  ft:{
+    mintAmount: 1000,
+    transferAmount:100,
+    burnAmount:50
+  },
+  nft:{
+    tokenUriOne: 'widget01',
+    tokenUriTwo: 'widget02',
+    tokenUriThree:'widget03'
+  },
+  ft_commitment:{
+    mintTokenOne: 10,
+    mintTokenTwo: 20,
+    transferAmount:15,
+    burnAmount:15
+  },
+  databaseConfig:{
+    host: 'nightfall_mongo_1',
+    port: "27017",
+    databaseName: "nightfall",
+    admin: "admin",
+    password: "admin"
+  },
 }
