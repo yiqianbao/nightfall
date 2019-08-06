84b802acf2e9e352a2cc51037ff1c8a2399a9912
asish.prema <asish.prema@gds.ey.com>
2019-08-06 12:00:09 +0530

    esllint and prettier configuration
    esllint and prettier configuration

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+  extends: ['codfish', 'codfish/docker', 'codfish/dapp'],
+  root: true,
+  env: {
+    node: true,
+  },
+  rules: {
+    'no-console': 'off',
+    'space-before-function-paren': ['error', {
+      'asyncArrow': 'always',
+      'anonymous': 'never',
+      'named': 'never'
+    }],
+    'radix': 'off',
+    'func-names': ['error', 'as-needed'],
+    'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
+    'comma-dangle': ['error', 'always-multiline'],
+    'semi': 'error',
+    'no-extra-semi': 'error',
+    'indent': ['error', 2, { "SwitchCase": 1 }],
+    'func-names': ['error', 'as-needed']
+  },
+};
