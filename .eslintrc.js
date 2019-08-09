762c5422745f9f4959b66c17834e7e991b1134ca
asish.prema <asish.prema@gds.ey.com>
2019-08-09 15:49:26 +0530

    fix: removed opinionated rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   extends: ['codfish', 'codfish/docker', 'codfish/dapp'],
   root: true,
@@ -7,17 +6,5 @@ module.exports = {
   },
   rules: {
     'no-console': 'off',
-    'space-before-function-paren': ['error', {
-      'asyncArrow': 'always',
-      'anonymous': 'never',
-      'named': 'never'
-    }],
-    'func-names': ['error', 'as-needed'],
-    'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
-    'comma-dangle': ['error', 'always-multiline'],
-    'semi': 'error',
-    'no-extra-semi': 'error',
-    'indent': ['error', 2, { "SwitchCase": 1 }],
-    'func-names': ['error', 'as-needed']
-  },
+  }
 };
