4678dc0c9a4ce132ba95a30d275c7a7c2320cbf9
asish.prema <asish.prema@gds.ey.com>
2019-08-20 10:24:33 +0530

    fixes(dot-docker): Removed virtual-hosts from docker compose files since we are no longer depended on dot docker. Changed api-gateway endpoint in integration test config file Removed --publicHost attribute from package.json Replaced with new port in protractor config file.

--- ui/protractor.conf.js
+++ ui/protractor.conf.js
@@ -10,7 +10,7 @@ exports.config = {
     browserName: 'chrome',
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://localhost:8000/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
