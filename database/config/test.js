74f7bd38e48b815cc5986ac86bb7bb5457ff3b1f
Liju Jose <liju.jose@gds.ey.com>
2019-09-18 12:30:47 +0530

    refactor(database): config folder shared as volume in docker-compose yml file

---
+++ database/config/test.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mongo: {
+    host: 'mongo_test',
+    port: '27017',
+    databaseName: 'nightfall_test',
+    admin: 'admin',
+    adminPassword: 'admin',
+  },
+  isLoggerEnable: true,
+}
