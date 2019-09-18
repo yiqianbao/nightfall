e4a46c566fed81df21f899009a01d6b9e8810d8b
Liju Jose <liju.jose@gds.ey.com>
2019-09-18 22:22:37 +0530

    refactor(offchain): config/ folder shared as volume

---
+++ offchain/config/default.js
@@ -0,0 +1,16 @@
+import { deferConfig as defer } from 'config/defer';
+
+module.exports = {
+  web3Host: process.env.BLOCKCHAIN_HOST,
+  web3Port: process.env.BLOCKCHAIN_PORT,
+  web3ProviderURL: defer(function getWeb3ProviderURL() {
+    return `${this.web3Host}:${this.web3Port}`;
+  }),
+  authenticationApi: {
+    host: process.env.AUTHENTICATION_API_HOST,
+    port: process.env.AUTHENTICATION_API_PORT,
+    url: defer(function getauthenticationApiURL() {
+      return `${this.authenticationApi.host}:${this.authenticationApi.port}`;
+    }),
+  }
+};
