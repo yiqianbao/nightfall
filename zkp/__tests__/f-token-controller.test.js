506186410a985fab7f9c139b672c8156159c8587
westlad <duncan.westland@uk.ey.com>
2019-10-25 09:08:57 +0100

    fix(zkp): fix test files

--- zkp/__tests__/f-token-controller.test.js
+++ zkp/__tests__/f-token-controller.test.js
@@ -1,44 +1,62 @@
 /* eslint-disable import/no-unresolved */
 
 import utils from 'zkp-utils';
-import Web3 from '../src/web3';
+import AccountUtils from '../src/account-utils/account-utils';
 
 import controller from '../src/f-token-controller';
+import vk from '../src/vk-controller';
 
 jest.setTimeout(7200000);
 
-Web3.connect();
-const web3 = Web3.connection();
+const C = '0x00000000000000000000000000000020'; // 128 bits = 16 bytes = 32 chars
+const D = '0x00000000000000000000000000000030';
+const E = '0x00000000000000000000000000000040';
+const F = '0x00000000000000000000000000000010'; // don't forget to make C+D=E+F
+const G = '0x00000000000000000000000000000030';
+const H = '0x00000000000000000000000000000020'; // these constants used to enable a second transfer
+const I = '0x00000000000000000000000000000050';
+const skA = '0x0000000000111111111111111111111111111111111111111111111111111111';
+const skB = '0x0000000000222222222222222222222222222222222222222222222222222222';
+let S_A_C;
+let S_A_D;
+let sAToBE;
+let sAToAF;
+let pkA;
+let pkB;
+const pkE = '0x0000000000111111111111111111111111111111111111111111111111111112';
+let Z_A_C;
+let Z_A_D;
+let S_B_G;
+let sBToEH;
+let sBToBI;
+let Z_B_G;
+let Z_B_E;
+let Z_A_F;
+// storage for z indexes
+let zInd1;
+let zInd2;
+let zInd3;
+
+beforeAll(async () => {
+  await vk.runController;
+  S_A_C = await utils.rndHex(32);
+  S_A_D = await utils.rndHex(32);
+  sAToBE = await utils.rndHex(32);
+  sAToAF = await utils.rndHex(32);
+  pkA = utils.ensure0x(utils.strip0x(utils.hash(skA)).padStart(32, '0'));
+  pkB = utils.ensure0x(utils.strip0x(utils.hash(skB)).padStart(32, '0'));
+  Z_A_C = utils.concatenateThenHash(C, pkA, S_A_C);
+  Z_A_D = utils.concatenateThenHash(D, pkA, S_A_D);
+  S_B_G = await utils.rndHex(32);
+  sBToEH = await utils.rndHex(32);
+  sBToBI = await utils.rndHex(32);
+  Z_B_G = utils.concatenateThenHash(G, pkB, S_B_G);
+  Z_B_E = utils.concatenateThenHash(E, pkB, sAToBE);
+  Z_A_F = utils.concatenateThenHash(F, pkA, sAToAF);
+});
 
 // eslint-disable-next-line no-undef
 describe('f-token-controller.js tests', () => {
-  const C = '0x00000000000000000000000000000020'; // 128 bits = 16 bytes = 32 chars
-  const D = '0x00000000000000000000000000000030';
-  const E = '0x00000000000000000000000000000040';
-  const F = '0x00000000000000000000000000000010'; // don't forget to make C+D=E+F
-  const skA = '0x111111111111111111111111111111111111111111111111111111';
-  const skB = '0x222222222222222222222222222222222222222222222222222222';
-  const S_A_C = '0x666666666666666666666666666666666666666666666666666666';
-  const S_A_D = '0x333333333333333333333333333333333333333333333333333333';
-  const sAToBE = '0x444444444444444444444444444444444444444444444444444444';
-  const sAToAF = '0x555555555555555555555555555555555555555555555555555555';
-  const pkA = utils.hash(skA);
-  const pkB = utils.hash(skB);
-  const Z_A_C = utils.concatenateThenHash(C, pkA, S_A_C);
-  const Z_A_D = utils.concatenateThenHash(D, pkA, S_A_D);
-  // these constants used to enable a second transfer:
-  const G = '0x00000000000000000000000000000030';
-  const H = '0x00000000000000000000000000000020';
-  const I = '0x00000000000000000000000000000050';
-  const S_B_G = '0x777777777777777777777777777777777777777777777777777777';
-  const sBToEH = '0x888888888888888888888888888888888888888888888888888888';
-  const sBToBI = '0x999999999999999999999999999999999999999999999999999999';
-  const Z_B_G = utils.concatenateThenHash(G, pkB, S_B_G);
-  const Z_B_E = utils.concatenateThenHash(E, pkB, sAToBE);
-  const pkE = '0x111111111111111111111111111111111111111111111111111112';
-  // And a burn
-  const Z_A_F = utils.concatenateThenHash(F, pkA, sAToAF);
-
   // Alice has C + D to start total = 50 ETH
   // Alice sends Bob E and gets F back (Bob has 40 ETH, Alice has 10 ETH)
   // Bob then has E+G at total of 70 ETH
@@ -46,7 +64,7 @@ describe('f-token-controller.js tests', () => {
 
   test('Should create 10000 tokens in accounts[0] and accounts[1]', async () => {
     // fund some accounts with FToken
-    const accounts = await web3.eth.getAccounts();
+    const accounts = await AccountUtils.getEthAccounts();
     const AMOUNT = 10000;
     const bal1 = await controller.getBalance(accounts[0]);
     await controller.buyFToken(AMOUNT, accounts[0]);
@@ -57,7 +75,7 @@ describe('f-token-controller.js tests', () => {
 
   test('Should move 1 ERC-20 token from accounts[0] to accounts[1]', async () => {
     const AMOUNT = 1;
-    const accounts = await web3.eth.getAccounts();
+    const accounts = await AccountUtils.getEthAccounts();
     const bal1 = await controller.getBalance(accounts[0]);
     const bal3 = await controller.getBalance(accounts[1]);
     await controller.transferFToken(AMOUNT, accounts[0], accounts[1]);
@@ -69,7 +87,7 @@ describe('f-token-controller.js tests', () => {
 
   test('Should burn 1 ERC-20 from accounts[1]', async () => {
     const AMOUNT = 1;
-    const accounts = await web3.eth.getAccounts();
+    const accounts = await AccountUtils.getEthAccounts();
     const bal1 = await controller.getBalance(accounts[1]);
     await controller.burnFToken(AMOUNT, accounts[1]);
     const bal2 = await controller.getBalance(accounts[1]);
@@ -77,34 +95,32 @@ describe('f-token-controller.js tests', () => {
   });
 
   test('Should get the ERC-20 metadata', async () => {
-    const accounts = await web3.eth.getAccounts();
+    const accounts = await AccountUtils.getEthAccounts();
     const { symbol, name } = await controller.getTokenInfo(accounts[0]);
     expect('OPS').toEqual(symbol);
     expect('EY OpsCoin').toEqual(name);
   });
 
   test('Should mint an ERC-20 commitment Z_A_C for Alice for asset C', async () => {
-    const accounts = await web3.eth.getAccounts();
+    const accounts = await AccountUtils.getEthAccounts();
     console.log('Alices account ', (await controller.getBalance(accounts[0])).toNumber());
     const [zTest, zIndex] = await controller.mint(C, pkA, S_A_C, accounts[0]);
-
+    zInd1 = parseInt(zIndex, 10);
     expect(Z_A_C).toEqual(zTest);
-    expect(0).toEqual(parseInt(zIndex, 10));
     console.log(`Alice's account `, (await controller.getBalance(accounts[0])).toNumber());
   });
 
   test('Should mint another ERC-20 commitment Z_A_D for Alice for asset D', async () => {
-    const accounts = await web3.eth.getAccounts();
+    const accounts = await AccountUtils.getEthAccounts();
     const [zTest, zIndex] = await controller.mint(D, pkA, S_A_D, accounts[0]);
-
+    zInd2 = parseInt(zIndex, 10);
     expect(Z_A_D).toEqual(zTest);
-    expect(1).toEqual(parseInt(zIndex, 10));
     console.log(`Alice's account `, (await controller.getBalance(accounts[0])).toNumber());
   });
 
   test('Should transfer a ERC-20 commitment to Bob (two coins get nullified, two created; one coin goes to Bob, the other goes back to Alice as change)', async () => {
     // E becomes Bob's, F is change returned to Alice
-    const accounts = await web3.eth.getAccounts();
+    const accounts = await AccountUtils.getEthAccounts();
     await controller.transfer(
       C,
       D,
@@ -117,25 +133,24 @@ describe('f-token-controller.js tests', () => {
       sAToAF,
       skA,
       Z_A_C,
-      0,
+      zInd1,
       Z_A_D,
-      1,
+      zInd2,
       accounts[0],
     );
     // now Bob should have 40 (E) ETH
   });
 
   test('Should mint another ERC-20 commitment Z_B_G for Bob for asset G', async () => {
-    const accounts = await web3.eth.getAccounts();
+    const accounts = await AccountUtils.getEthAccounts();
     const [zTest, zIndex] = await controller.mint(G, pkB, S_B_G, accounts[1]);
-
+    zInd3 = parseInt(zIndex, 10);
     expect(Z_B_G).toEqual(zTest);
-    expect(4).toEqual(parseInt(zIndex, 10));
   });
 
   test('Should transfer an ERC-20 commitment to Eve', async () => {
     // H becomes Eve's, I is change returned to Bob
-    const accounts = await web3.eth.getAccounts();
+    const accounts = await AccountUtils.getEthAccounts();
     await controller.transfer(
       E,
       G,
@@ -148,20 +163,20 @@ describe('f-token-controller.js tests', () => {
       sBToBI,
       skB,
       Z_B_E,
-      2,
+      zInd1 + 2,
       Z_B_G,
-      4,
+      zInd3,
       accounts[1],
     );
   });
 
   test(`Should burn Alice's remaining ERC-20 commitment`, async () => {
-    const accounts = await web3.eth.getAccounts();
+    const accounts = await AccountUtils.getEthAccounts();
     const bal1 = await controller.getBalance(accounts[3]);
     const bal = await controller.getBalance(accounts[0]);
     console.log('accounts[3]', bal1.toNumber());
     console.log('accounts[0]', bal.toNumber());
-    await controller.burn(F, skA, sAToAF, Z_A_F, 3, accounts[0], accounts[3]);
+    await controller.burn(F, skA, sAToAF, Z_A_F, zInd2 + 2, accounts[0], accounts[3]);
     const bal2 = await controller.getBalance(accounts[3]);
     console.log('accounts[3]', bal2.toNumber());
     expect(parseInt(F, 16)).toEqual(bal2 - bal1);
--- zkp/__tests__/nf-token-controller.test.js
+++ zkp/__tests__/nf-token-controller.test.js
@@ -1,122 +1,142 @@
 /* eslint-disable import/no-unresolved */
 
 import utils from 'zkp-utils';
-import config from 'config';
+import AccountUtils from '../src/account-utils/account-utils';
 import controller from '../src/nf-token-controller';
-import Web3 from '../src/web3';
+import vk from '../src/vk-controller';
 
 jest.setTimeout(7200000);
 
-Web3.connect();
-const web3 = Web3.connection();
+let A;
+let B;
+let G;
+const skA = '0x0000000000111111111111111111111111111111111111111111111111111111';
+const skB = '0x0000000000222222222222222222222222222222222222222222222222222222';
+const A_URI = 'Pizza';
+let pkA;
+let pkB;
+let S_A_A;
+let S_A_G;
+let sAToBA;
+let sAToBG;
+let Z_A_A;
+let Z_A_G;
+let Z_B_A;
+let Z_B_G;
+let zIndA;
+let zIndG;
+
+beforeAll(async () => {
+  await vk.runController;
+  A = await utils.rndHex(32);
+  B = await utils.rndHex(32);
+  G = await utils.rndHex(32);
+  pkA = utils.ensure0x(utils.strip0x(utils.hash(skA)).padStart(32, '0'));
+  pkB = utils.ensure0x(utils.strip0x(utils.hash(skB)).padStart(32, '0'));
+  S_A_A = await utils.rndHex(32);
+  S_A_G = await utils.rndHex(32);
+  sAToBA = await utils.rndHex(32);
+  sAToBG = await utils.rndHex(32);
+  Z_A_A = utils.concatenateThenHash(utils.strip0x(A).slice(-32 * 2), pkA, S_A_A);
+  Z_A_G = utils.concatenateThenHash(utils.strip0x(G).slice(-32 * 2), pkA, S_A_G);
+  Z_B_A = utils.concatenateThenHash(utils.strip0x(A).slice(-32 * 2), pkB, sAToBA);
+  Z_B_G = utils.concatenateThenHash(utils.strip0x(G).slice(-32 * 2), pkB, sAToBG);
+});
 
 describe('nf-token-controller.js tests', () => {
-  const A = '0x422ded6ca3e8ccf66773e1701194ef8818cfade93b351c846b1619cd6fff4e53';
-  const B = '0x33d4286234bcc9ecf4ebd4323f98c62d8858c02c70a71ab6bf61487439cbea65';
-  const G = '0xadd66b07a769e5753a227f724bf32ce8af261b838f2e850416a2bc958dcb9386';
-  const skA = '0x111111111111111111111111111111111111111111111111111111';
-  const skB = '0x222222222222222222222222222222222222222222222222222222';
-  const pkA = utils.hash(skA);
-  const pkB = utils.hash(skB);
-  const S_A_A = '0x333333333333333333333333333333333333333333333333333333';
-  const S_A_G = '0x333333333333333133333333333333313333333333333331333333';
-  const sAToBA = '0x444444444444444444444444444444444444444444444444444444';
-  const sAToBG = '0xefefadecb1000000efefadecb1000000efefadecb1000000000000';
-  const A_URI = 'Pizza';
-
-  const Z_A_A = utils.concatenateThenHash(
-    utils.strip0x(A).slice(-(config.INPUTS_HASHLENGTH * 2)),
-    pkA,
-    S_A_A,
-  );
-
-  const Z_A_G = utils.concatenateThenHash(
-    utils.strip0x(G).slice(-(config.INPUTS_HASHLENGTH * 2)),
-    pkA,
-    S_A_G,
-  );
-
-  const Z_B_A = utils.concatenateThenHash(
-    utils.strip0x(A).slice(-(config.INPUTS_HASHLENGTH * 2)),
-    pkB,
-    sAToBA,
-  );
-
-  const Z_B_G = utils.concatenateThenHash(
-    utils.strip0x(G).slice(-(config.INPUTS_HASHLENGTH * 2)),
-    pkB,
-    sAToBG,
-  );
-
   test('Should mint ERC 721 token for Alice for asset A', async () => {
-    const accounts = await web3.eth.getAccounts();
+    const accounts = await AccountUtils.getEthAccounts();
     await controller.mintNFToken(A, A_URI, accounts[0]);
     expect((await controller.getOwner(A, '')).toLowerCase()).toEqual(accounts[0].toLowerCase());
     expect(await controller.getNFTURI(A, '')).toEqual(A_URI);
   });
 
   test('Should mint ERC 721 token for Alice for asset B', async () => {
-    const accounts = await web3.eth.getAccounts();
+    const accounts = await AccountUtils.getEthAccounts();
     await controller.mintNFToken(B, '', accounts[0]);
     expect((await controller.getOwner(B, '')).toLowerCase()).toEqual(accounts[0].toLowerCase());
   });
 
   test('Should add Eve as approver for ERC 721 token for asset B', async () => {
-    const accounts = await web3.eth.getAccounts();
+    const accounts = await AccountUtils.getEthAccounts();
     await controller.addApproverNFToken(accounts[2], B, accounts[0]);
     expect((await controller.getApproved(B, '')).toLowerCase()).toEqual(accounts[2].toLowerCase());
   });
 
   test('Should mint ERC 721 token for Alice for asset G', async () => {
-    const accounts = await web3.eth.getAccounts();
+    const accounts = await AccountUtils.getEthAccounts();
     await controller.mintNFToken(G, '', accounts[0]);
     expect((await controller.getOwner(G, '')).toLowerCase()).toEqual(accounts[0].toLowerCase());
   });
 
   test('Should transfer ERC 721 token B from Alice to Bob', async () => {
-    const accounts = await web3.eth.getAccounts();
+    const accounts = await AccountUtils.getEthAccounts();
     await controller.transferNFToken(B, accounts[0], accounts[2]);
     expect((await controller.getOwner(B, '')).toLowerCase()).toEqual(accounts[2].toLowerCase());
   });
 
   test('Should burn ERC 721 token B of Bob', async () => {
-    const accounts = await web3.eth.getAccounts();
+    const accounts = await AccountUtils.getEthAccounts();
     const countBefore = await controller.getBalance(accounts[2]);
     await controller.burnNFToken(B, accounts[2]);
     expect((await controller.getBalance(accounts[2])).toNumber()).toEqual(countBefore - 1);
   });
 
   test('Should mint an ERC 721 commitment for Alice for asset A  (Z_A_A)', async () => {
-    const accounts = await web3.eth.getAccounts();
+    const accounts = await AccountUtils.getEthAccounts();
     const [zTest, zIndex] = await controller.mint(A, pkA, S_A_A, accounts[0]);
+    zIndA = parseInt(zIndex, 10);
     expect(Z_A_A).toEqual(zTest);
-    expect(parseInt(zIndex, 10)).toEqual(0);
   });
 
   test('Should mint an ERC 721 commitment for Alice for asset G (Z_A_G)', async () => {
-    const accounts = await web3.eth.getAccounts();
+    const accounts = await AccountUtils.getEthAccounts();
     const [zTest, zIndex] = await controller.mint(G, pkA, S_A_G, accounts[0]);
+    zIndG = parseInt(zIndex, 10);
     expect(Z_A_G).toEqual(zTest);
-    expect(parseInt(zIndex, 10)).toEqual(1);
   });
 
   test('Should transfer the ERC 721 commitment Z_A_A from Alice to Bob, creating Z_B_A', async () => {
-    const accounts = await web3.eth.getAccounts();
-    const response = await controller.transfer(A, pkB, S_A_A, sAToBA, skA, Z_A_A, 0, accounts[0]);
+    const accounts = await AccountUtils.getEthAccounts();
+    const response = await controller.transfer(
+      A,
+      pkB,
+      S_A_A,
+      sAToBA,
+      skA,
+      Z_A_A,
+      zIndA,
+      accounts[0],
+    );
     expect(response.z_B).toEqual(Z_B_A);
-    expect(parseInt(response.z_B_index, 10)).toEqual(2);
   });
 
   test('Should transfer the ERC 721 commitment Z_A_G from Alice to Bob, creating Z_B_G', async () => {
-    const accounts = await web3.eth.getAccounts();
-    const response = await controller.transfer(G, pkB, S_A_G, sAToBG, skA, Z_A_G, 1, accounts[0]);
+    const accounts = await AccountUtils.getEthAccounts();
+    const response = await controller.transfer(
+      G,
+      pkB,
+      S_A_G,
+      sAToBG,
+      skA,
+      Z_A_G,
+      zIndG,
+      accounts[0],
+    );
     expect(response.z_B).toEqual(Z_B_G);
-    expect(parseInt(response.z_B_index, 10)).toEqual(3);
   });
 
   test('Should burn the ERC 721 commitment for Bob for asset Z_B_A to return A ERC-721 Token', async () => {
-    const accounts = await web3.eth.getAccounts();
-    const response = await controller.burn(A, skB, sAToBA, Z_B_A, 2, accounts[2], accounts[2]);
+    const accounts = await AccountUtils.getEthAccounts();
+    const response = await controller.burn(
+      A,
+      skB,
+      sAToBA,
+      Z_B_A,
+      zIndA + 2,
+      accounts[2],
+      accounts[2],
+    );
     expect(Z_B_A).toEqual(response.z_A);
     expect((await controller.getOwner(A, '')).toLowerCase()).toEqual(accounts[2].toLowerCase());
   });
