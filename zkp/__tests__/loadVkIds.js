9a5c815969f73361d86e6501ffb5cd67f69c4604
Liju Jose <36976825+LijuJoseJJ@users.noreply.github.com>
2019-11-14 17:32:34 +0530

    unit test now load vkIds once (#294)
    * refactor(zkp-unit-test): vkIds load for once
    
    * refactor(zkp-unit-test): verbose flag added

--- zkp/__tests__/loadVkIds.js
+++ zkp/__tests__/loadVkIds.js
@@ -1,7 +1,7 @@
 import bc from '../src/web3';
 import vk from '../src/vk-controller';
 
-global.loadVkIds = async function loadVkIds() {
+module.exports = async function loadVkIds() {
   if (!(await bc.isConnected())) return;
   await vk.runController();
 };
