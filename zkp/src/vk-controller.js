923546ee284b632ab2c352eac4b5176cef96fb03
Will Kim <william.kim@ey.com>
2019-12-20 12:16:57 -0500

    feat: add loadVk from nightlite (#350)

--- zkp/src/vk-controller.js
+++ zkp/src/vk-controller.js
@@ -5,69 +5,13 @@
 rest api calls, and the heavy-lifitng token-zkp.js and zokrates.js.  It exists so that the amount of logic in restapi.js is absolutely minimised.
 */
 
-import contract from 'truffle-contract';
-import jsonfile from 'jsonfile';
 import fs from 'fs';
 import config from 'config';
-import utils from './zkpUtils';
+import { vks } from '@eyblockchain/nightlite';
 import Web3 from './web3';
 import { getTruffleContractInstance } from './contractUtils';
 
 const web3 = Web3.connection();
-
-/**
-Loads a verification key to the Verifier Registry
- * @param {String} vkJsonFile - Path to vk file in JSON form
- * @param {Object} blockchainOptions
- * @param {Object} blockchainOptions.verifierJson - Compiled JSON of verifier contract
- * @param {String} blockchainOptions.verifierAddress - address of deployed verifier contract
- * @param {Object} blockchainOptions.verifierRegistryJson - Compiled JSON of verifier contract
- * @param {String} blockchainOptions.verifierRegistryAddress - address of deployed verifier contract
- * @param {String} blockchainOptions.account - Account that will send the transactions
-*/
-async function loadVk(vkJsonFile, blockchainOptions) {
-  const {
-    verifierJson,
-    verifierAddress,
-    verifierRegistryJson,
-    verifierRegistryAddress,
-    account,
-  } = blockchainOptions;
-
-  console.log(`Loading VK for ${vkJsonFile}`);
-
-  const verifier = contract(verifierJson);
-  verifier.setProvider(Web3.connect());
-  const verifierInstance = await verifier.at(verifierAddress);
-
-  const verifierRegistry = contract(verifierRegistryJson);
-  verifierRegistry.setProvider(Web3.connect());
-  const verifierRegistryInstance = await verifierRegistry.at(verifierRegistryAddress);
-
-  // Get VKs from the /code/gm17 directory and convert them into Solidity uints.
-  let vk = await new Promise((resolve, reject) => {
-    jsonfile.readFile(vkJsonFile, (err, data) => {
-      if (err) reject(err);
-      else resolve(data);
-    });
-  });
-  vk = Object.values(vk);
-  vk = utils.flattenDeep(vk);
-  vk = vk.map(el => utils.hexToDec(el));
-
-  // upload the vk to the smart contract
-  console.log('Registering verifying key');
-  const txReceipt = await verifierRegistryInstance.registerVk(vk, [verifierInstance.address], {
-    from: account,
-    gas: 6500000,
-    gasPrice: config.GASPRICE,
-  });
-
-  // eslint-disable-next-line no-underscore-dangle
-  const vkId = txReceipt.logs[0].args._vkId;
-
-  return vkId;
-}
 
 /**
  * Loads VKs to the VerifierRegistry, saves the VkIds to a JSON file, then submits the VkIds to the Shield contracts
@@ -99,13 +43,13 @@ async function initializeVks() {
   // Load VK to VerifierRegistry and get back vkIds
   try {
     ids = await Promise.all([
-      loadVk(config.NFT_MINT_VK, blockchainOptions),
-      loadVk(config.NFT_TRANSFER_VK, blockchainOptions),
-      loadVk(config.NFT_BURN_VK, blockchainOptions),
-      loadVk(config.FT_MINT_VK, blockchainOptions),
-      loadVk(config.FT_TRANSFER_VK, blockchainOptions),
-      loadVk(config.FT_SIMPLE_BATCH_TRANSFER_VK, blockchainOptions),
-      loadVk(config.FT_BURN_VK, blockchainOptions),
+      vks.loadVk(config.NFT_MINT_VK, blockchainOptions),
+      vks.loadVk(config.NFT_TRANSFER_VK, blockchainOptions),
+      vks.loadVk(config.NFT_BURN_VK, blockchainOptions),
+      vks.loadVk(config.FT_MINT_VK, blockchainOptions),
+      vks.loadVk(config.FT_TRANSFER_VK, blockchainOptions),
+      vks.loadVk(config.FT_SIMPLE_BATCH_TRANSFER_VK, blockchainOptions),
+      vks.loadVk(config.FT_BURN_VK, blockchainOptions),
     ]);
   } catch (err) {
     throw new Error('Error while loading VKs', err);
