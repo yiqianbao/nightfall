9c472b3cd1e0fcf74d981502f48adec288335579
HenryFour <me@henryfour.com>
2020-04-13 14:59:39 +0800

    update custom-geth

--- custom-geth/init.sh
+++ custom-geth/init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# once init
+# geth makedag 0 ./datadir/dag
+# geth makedag 30000 ./datadir/dag
 # geth --datadir ./datadir account new
+
 # 执行前需要确认已更新 genesis.json 的 coinbase, alloc 账户
 rm -fr ./datadir/geth
 geth --datadir ./datadir init genesis.json
--- custom-geth/run.sh
+++ custom-geth/run.sh
@@ -3,8 +3,8 @@
 ./geth --datadir ./datadir \
     --unlock 0x4c34Ac28a0707cF3289e288126452Fcb5A03830e --password ./pass.txt --allow-insecure-unlock \
     --rpc --rpcaddr 0.0.0.0 --rpcvhosts "*" --rpccorsdomain "*" --rpcport 8545 --rpcapi eth,web3,admin,personal,net --allow-insecure-unlock \
-    --ws --wsaddr 0.0.0.0 --wsorigins "*" --wsport 8546 --wsapi eth,web3,admin,personal,net --networkid 999 \
-    --nodiscover --mine \
+    --ws --wsaddr 0.0.0.0 --wsorigins "*" --wsport 8546 --wsapi eth,web3,admin,personal,net \
+    --networkid 999 --nodiscover --mine --miner.threads=2 --ethash.dagdir ./datadir/dag \
     --verbosity 5 --vmdebug 
 
 # --verbosity 3
